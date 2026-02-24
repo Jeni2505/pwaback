3bd4c76b701e9c8419c1c1da47a923fe6cc0fbb1
Jeni2505 <2123200440@soy.utj.edu.mx>
2026-02-24 14:58:22 -0600

    fix: configurar backend

--- src/app.js
+++ src/app.js
@@ -1,44 +1,29 @@
-// src/app.js
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
-import { connectToDB } from "./bd/connect.js";
+import { connectToDB } from "./db/connect.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      process.env.FRONT_ORIGIN || ""
-    ].filter(Boolean),
-    credentials: true,
-  })
-);
 
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        process.env.FRONT_ORIGIN || ""
+    ].filter(Boolean),
+    credentials: true
+})
+
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
-// Conexión a MongoDB (serverless safe)
+//conexion a mongo DB
 app.use(async (_req, _res, next) => {
-  try {
-    await connectToDB();
-    next();
-  } catch (error) {
-    next(error);
-  }
+    try {
+        await connectToDB(); next();
+    } catch (e) { next(e);}
 });
 
-// Ruta de prueba
-app.get("/", (req, res) => {
-  res.json({ ok: true, message: "API funcionando 🚀" });
-});
-
-// Rutas
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
-
-export default app;
