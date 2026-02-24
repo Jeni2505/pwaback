ea03d247ce81116d46b1018824968335ba3e448b
Jeni2505 <2123200440@soy.utj.edu.mx>
2026-02-24 14:53:31 -0600

    fix: configurar backend para vercel serverles

--- src/server.js
+++ src/server.js
@@ -1,31 +1,19 @@
-import 'dotenv/config';
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import morgan from 'morgan';
+// src/server.js
+import "dotenv/config";
+import mongoose from "mongoose";
+import app from "./app.js";
 
+const { PORT = 4000, MONGO_URI } = process.env;
 
-import authRoutes from './routes/auth.routes.js';
-import taskRoutes from './routes/task.routes.js';
-
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(morgan('dev'));
-
-
-app.get('/', (req, res) => res.json({ok: true, name: 'Jenifer Todo API'}));
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
-
-const {PORT = 4000, MONGO_URI} = process.env;
-mongoose.connect(process.env.MONGO_URL, {dbName: 'BackPWA'})
-    .then(() => {
-        console.log('Conectado a mongoDB', mongoose.connection.name);
-        app.listen(PORT, () => console.log(`Servidor ejecutandose por: ${PORT}`));
-    })
-    .catch(err =>{
-        console.error('Error conectado a mongoDB', err);
-        process.exit(1);
+mongoose
+  .connect(MONGO_URI, { dbName: "BackPWA" })
+  .then(() => {
+    console.log("Conectado a MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Servidor ejecutándose en puerto ${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error("Error conectando a MongoDB", err);
+    process.exit(1);
+  });
