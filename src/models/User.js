ab5531389b4005bbf8a358888581443a17a92d65
Jeni2505 <2123200440@soy.utj.edu.mx>
2026-02-27 19:53:20 -0600

    back como lo deje funcionando

--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-{
-    name: { type: String, required: true, trim: true },
-    email: { type: String, required: true, trim: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
-
-    // 🔹 NUEVO: Rol
-    role: {
-        type: String,
-        enum: ["user", "admin"],
-        default: "user"
+    {
+        name: { type: String, required: true, trim: true }, //sirve para que los datos no tengan espacios en blanco al inicio o al final y no caduquen
+        email: { type: String, required: true, trim: true, unique: true, lowercase: true },
+        password: { type: String, required: true },
     },
-
-    // 🔹 NUEVO: Última actividad
-    lastActive: {
-        type: Date,
-        default: Date.now
+    { 
+        timestamps: true 
     }
-},
-{
-    timestamps: true
-}
 );
 
 export default mongoose.model("User", userSchema);
